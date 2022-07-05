d6102cb6dc68ca323cccd499bd919432cc5e4d4f
yuval-david <yuvid0224@gmail.com>
2022-07-05 15:55:58 +0300

    Updates

---
+++ precache-manifest.720b5a6acc5bd5db93b09a4795184d4f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833c68fb915f493f17c9c812d66727b0",
+    "url": "/K.C-Beauty/index.html"
+  },
+  {
+    "revision": "952c896a786fad598e00",
+    "url": "/K.C-Beauty/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "7dfba694af6e545bcdb3",
+    "url": "/K.C-Beauty/static/css/main.633690d5.chunk.css"
+  },
+  {
+    "revision": "952c896a786fad598e00",
+    "url": "/K.C-Beauty/static/js/2.d2cd1c72.chunk.js"
+  },
+  {
+    "revision": "fb7b229515dfeef35b4dfd848e0f4f7e",
+    "url": "/K.C-Beauty/static/js/2.d2cd1c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dfba694af6e545bcdb3",
+    "url": "/K.C-Beauty/static/js/main.ce05acf9.chunk.js"
+  },
+  {
+    "revision": "1edf1f3f4faa958efbdf",
+    "url": "/K.C-Beauty/static/js/runtime-main.c7f4dd57.js"
+  },
+  {
+    "revision": "68b2a26b4c4326dc762d63858f4bd377",
+    "url": "/K.C-Beauty/static/media/0.68b2a26b.png"
+  },
+  {
+    "revision": "d700affd5f01688869a0f6c29c236be5",
+    "url": "/K.C-Beauty/static/media/1.d700affd.png"
+  },
+  {
+    "revision": "2b6e3be3f3319601e0db8c4ac53cb7f3",
+    "url": "/K.C-Beauty/static/media/2.2b6e3be3.png"
+  },
+  {
+    "revision": "fa553839492e3e859af03ce898428fb1",
+    "url": "/K.C-Beauty/static/media/3.fa553839.png"
+  },
+  {
+    "revision": "d1d4334ec168cdaf2bb0bd5b2f546c57",
+    "url": "/K.C-Beauty/static/media/4.d1d4334e.png"
+  },
+  {
+    "revision": "febe8459107f91abfcb6d1f20f3fdda6",
+    "url": "/K.C-Beauty/static/media/5.febe8459.png"
+  },
+  {
+    "revision": "ca150f4105603766d215f7eee59e2e50",
+    "url": "/K.C-Beauty/static/media/6.ca150f41.png"
+  },
+  {
+    "revision": "d747860c3562da43dec8a04fda41b0b1",
+    "url": "/K.C-Beauty/static/media/7.d747860c.png"
+  },
+  {
+    "revision": "aa54ec9765a3093261027dcb1b5aefd4",
+    "url": "/K.C-Beauty/static/media/8.aa54ec97.png"
+  },
+  {
+    "revision": "95f96c9c89da4b7db6904491d05593f4",
+    "url": "/K.C-Beauty/static/media/about-bg.95f96c9c.jpg"
+  },
+  {
+    "revision": "fd3fa3a39bc3afae0f3c9bbbc49b4f08",
+    "url": "/K.C-Beauty/static/media/about-nails.fd3fa3a3.jpg"
+  },
+  {
+    "revision": "623bed2ea0a3e141c348aa2985e5cea5",
+    "url": "/K.C-Beauty/static/media/nailss.623bed2e.jpeg"
+  },
+  {
+    "revision": "cee21bfc4be62a7dce958552d678aa14",
+    "url": "/K.C-Beauty/static/media/pinkNail.cee21bfc.jpeg"
+  },
+  {
+    "revision": "48c56b7b75951eb721b10ccdca881832",
+    "url": "/K.C-Beauty/static/media/redPhine.48c56b7b.jpg"
+  },
+  {
+    "revision": "ef3136805e3e1056d62a9914d2fa95ab",
+    "url": "/K.C-Beauty/static/media/wood.ef313680.jpeg"
+  }
+]);
